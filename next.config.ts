eee9d5ad5125e20598953ffe00854ee8a10a03a6
Semih babacan <semih.babacan@solace.com.tr>
2025-08-29 07:33:22 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
 const nextConfig: NextConfig = {
   eslint: {
@@ -19,14 +16,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
 };
 
 export default nextConfig;
